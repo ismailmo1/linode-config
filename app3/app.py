ead6c603ee3dae333f46c2bb1b1fe2c019a0043d
Ismail Mohammed <ismailmo4@gmail.com>
2022-03-30 23:49:41 +0100

    added app3

---
+++ app3/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({"hi": "im app3"})
+
+if __name__ =="__main__":
+    app.run(host='0.0.0.0')
